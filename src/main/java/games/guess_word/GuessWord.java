e4bfa962f6aa4a012ae546fa69f46cde84fbe29b
Andrei Sogomonian <saipeg@yandex.ru>
2022-01-08 21:43:01 +0300

    Worked with loops

--- src/main/java/games/guess_word/GuessWord.java
+++ src/main/java/games/guess_word/GuessWord.java
@@ -4,18 +4,25 @@ import java.io.IOException;
 
 public class GuessWord {
     public static void main(String[] args) throws IOException {
-        char myWord = 'd';
+        char guessWord, myWord = 'd';
+        int count = 0;
 
-        char guessWord = (char) System.in.read();
-        if (myWord == guessWord) {
-            System.out.println("Счастливчик, угадал!");
-        } else {
-            System.out.println("Не угадал, попробуй еще");
-            if (myWord < guessWord) {
-                System.out.println("буква ближе к началу алфавита");
+        while (count < 5) {
+            guessWord = (char) System.in.read();
+
+            if (myWord == guessWord) {
+                System.out.println("Счастливчик, угадал!");
+                break;
+
             } else {
-                System.out.println("буква ближе к концу алфавита");
+                System.out.println("Не угадал, попробуй еще");
+//                if (guessWord  < myWord ) {
+//                    System.out.println("буква ближе к началу алфавита");
+//                } else {
+//                    System.out.println("буква ближе к концу алфавита");
+//                }
             }
+            count++;
         }
     }
 }
---
+++ src/main/java/games/micro/AlphabetWithWhile.java
@@ -0,0 +1,11 @@
+package games.micro;
+
+public class AlphabetWithWhile {
+    public static void main(String[] args) {
+        char ch = 'a';
+        while(ch <= 'z') {
+            System.out.println(ch);
+            ch++;
+        }
+    }
+}
