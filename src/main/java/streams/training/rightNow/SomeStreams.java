26842c4307661dba7320c7de519104cdd5fcfa9e
Andrei Sogomonian <saipeg@yandex.ru>
2022-01-11 22:14:19 +0300

    Remember stream from array

--- src/main/java/streams/training/rightNow/SomeStreams.java
+++ src/main/java/streams/training/rightNow/SomeStreams.java
@@ -1,7 +1,19 @@
 package streams.training.rightNow;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class SomeStreams {
     public static void main(String[] args) {
+
+        int[] nums = {1, 33, 6, 44,-1, 44};
+
+        nums = Arrays.stream(nums)
+                .filter(x -> x > 0)
+                .sorted()
+                .toArray();
+        System.out.println(Arrays.toString(nums));
+
 
     }
 }
