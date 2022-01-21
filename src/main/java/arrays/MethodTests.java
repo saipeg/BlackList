d28d5d1db460968437f7926e8ae6fc494bba32c8
Andrei Sogomonian <saipeg@yandex.ru>
2022-01-21 10:43:57 +0300

    retainAll - up created collections

--- src/main/java/arrays/MethodTests.java
+++ src/main/java/arrays/MethodTests.java
@@ -6,17 +6,27 @@ import java.util.List;
 
 public class MethodTests {
     public static void main(String[] args) {
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>() {{
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+        }};
+        List<Integer> list2 = new ArrayList<>() {{
+            add(2);
+            add(3);
+        }};
+        List<Integer> list3 = new ArrayList<>(){{
+            add(2);
+        }};
 
-        Collections.addAll(list1, 1, 2, 3, 4, 5);
-        Collections.addAll(list2, 1, 2, 3);
+//        result.stream().distinct().forEach(x -> {
+//            if (list2.contains(x) && list3.contains(x))
+//                filtered.add(x);
+//        });
 
-        System.out.println("List1: " + list1.toString());
-        System.out.println("List2: " + list2.toString());
-
-        list1.retainAll(list2);
-        System.out.println("List1: " + list1.toString());
-
+        list2.retainAll(list3);
+        list2.retainAll(list1);
+        System.out.println("List1: " + list2);
     }
 }
