028a74831c17858a282aba72c57fbc8b6657a6a1
Andrei Sogomonian <saipeg@yandex.ru>
2022-02-12 17:30:47 +0300

    Tick Tock part.1

---
+++ src/main/java/threads/shild/tick_tock/TickTickRun.java
@@ -0,0 +1,9 @@
+package threads.shild.tick_tock;
+
+public class TickTickRun {
+    public static void main(String[] args) {
+        TickTock tickTock = new TickTock();
+
+
+    }
+}
--- src/main/java/threads/synchronizedExample/SynchronizedWithLock.java
+++ src/main/java/threads/synchronizedExample/SynchronizedWithLock.java
@@ -1,6 +1,5 @@
 package threads.synchronizedExample;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
