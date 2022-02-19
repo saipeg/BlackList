a1f7f7129e639bc6ce0d69699a7c4371737f5a1a
Andrei Sogomonian <saipeg@yandex.ru>
2022-02-19 16:20:15 +0300

    Fixed previous commit, Generics - from Shild.

--- src/main/java/generics/shild/MyGen.java
+++ src/main/java/generics/shild/MyGen.java
@@ -1,5 +1,38 @@
 package generics.shild;
 
-public class MyGen<T> {
+public class MyGen<T, V> {
+    T ob1;
+    V ob2;
+
+    public MyGen(T ob1, V ob2) {
+        this.ob1 = ob1;
+        this.ob2 = ob2;
+    }
+
+    void showTypes() {
+        System.out.println("Type T is: " + ob1.getClass().getName());
+        System.out.println("Type V is: " + ob1.getClass().getName());
+    }
+
+    T getOb1() {
+        return ob1;
+    }
+
+    V getOb2() {
+        return ob2;
+    }
+
+    public static void main(String[] args) {
+        MyGen<String, Integer> myGen = new MyGen<>("The first arg in my Generic", 12);
+
+        myGen.showTypes();
+
+        String valueStrFromMyGen = myGen.getOb1();
+        System.out.println("valueStrFromMyGen = " + valueStrFromMyGen);
+
+        int valueIntFromMyGen = myGen.getOb2();
+        System.out.println("valueIntFromMyGen = " + valueIntFromMyGen);
+
+    }
 
 }
