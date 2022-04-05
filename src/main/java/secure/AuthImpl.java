2d0858d3f6aa2f66787d776856578574862dd387
Andrei Sogomonian <saipeg@yandex.ru>
2022-04-05 17:00:02 +0300

    Added method generate Password With Policy

--- src/main/java/secure/AuthImpl.java
+++ src/main/java/secure/AuthImpl.java
@@ -1,6 +1,7 @@
 package secure;
 
 import java.security.SecureRandom;
+import java.util.Random;
 
 public class AuthImpl {
 
@@ -12,11 +13,31 @@ public class AuthImpl {
         return String.format("%05d", num); //make size to 5 symbols
     }
 
-    public static void main(String[] args) {
-
-        String password = AuthImpl.generateSmsPassword();
+    public static String generatePasswordWithPolicy() {
+        Random random = new Random();
+        StringBuilder password = new StringBuilder(8);
         System.out.println("password = " + password);
-
+        final String CHAR_SET = "0123456789ABCDEFGHIKLMNOPQRSTVXYZ";
+        for (int i = 0; i < 3; ++i) {
+            password.insert(i, new char[]{CHAR_SET.charAt(random.nextInt(CHAR_SET.length()))}, 0, 1);
+        }
+        return password.toString();
     }
 
-}
+        public static void main (String[]args){
+
+            String password = AuthImpl.generateSmsPassword();
+            System.out.println("password = " + password);
+
+            System.out.println("========================================");
+
+            String bigPass = AuthImpl.generatePasswordWithPolicy();
+            System.out.println("bigPass = " + bigPass);
+            System.out.println("length: " + bigPass.length());
+
+
+        }
+
+
+
+    }
