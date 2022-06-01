736db7c6880d241b1815f814f62801f225cc805f
Andrei Sogomonian <saipeg@yandex.ru>
2022-06-01 15:12:21 +0300

    integers pool checked

---
+++ src/main/java/threads/training/PoolIntsTesting.java
@@ -0,0 +1,16 @@
+package threads.training;
+
+public class PoolIntsTesting {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 145;
+
+        if (a == b) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+
+    }
+}
